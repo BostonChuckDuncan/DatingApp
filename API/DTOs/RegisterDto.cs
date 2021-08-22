38057dad3d4a25860014dd100ed5e97ae122285a
BostonChuck <cduncan@BostonDuncan.com>
2021-08-22 10:55:31 -0400

    AccountController RegisterUser, DTO

---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class RegisterDto
+    {
+        [Required]
+        [MinLength(3, ErrorMessage="user name minimum length of 3")]
+        public string Username { get; set; }
+        
+        [Required]
+        [MinLength(8, ErrorMessage = "password minimum length of 8")]
+        public string Password { get; set; }
+    }
+}
