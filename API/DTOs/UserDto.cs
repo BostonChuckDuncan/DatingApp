57fd2d98de44182aab70cc06a9907919d86f2a5f
BostonChuck <cduncan@BostonDuncan.com>
2021-08-22 12:20:59 -0400

    Added JWT - not in middleware yet

---
+++ API/DTOs/UserDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Principal;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace API.DTOs
+{
+    public class UserDto
+    {
+        public string Username { get; set; }
+        public string Token { get; set; }
+    }
+}
